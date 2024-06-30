0ea7ee4388ca4895137e8d2f73d7ccfddf501d0d
Hira-Yuki <cjsdlf44@gmail.com>
2024-06-30 18:38:03 +0900

    Created New Vite + React + TypeScript Project.
    .eslintrc.cjs : ESLint 설정 파일
    .prettierrc : Prettier  설정 파일
    .vscode/setting.json : vscode 설정 파일
    
    나머지 파일은 모두 vite로 프로젝트 생성시 기본 작성된 내용입니다.

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "no-var": "warn", // var 금지
+    "react/jsx-pascal-case": "warn", // 컴포넌트 이름은 PascalCase로
+    "react/jsx-key": "warn", // 반복문으로 생성하는 요소에 key 강제
+  },
+};
+
+// 기본 ESLint 설정
