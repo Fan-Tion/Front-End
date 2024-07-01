1ed31b3bf58ddc17bcfc568ad2277f04b372d836
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-01 22:52:54 +0900

    ESLint + package + CSS 재설정, 코드 스타일 정의

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,21 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended', // 이 줄 추가
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "no-var": "warn", // var 금지
-    "react/jsx-pascal-case": "warn", // 컴포넌트 이름은 PascalCase로
-    "react/jsx-key": "warn", // 반복문으로 생성하는 요소에 key 강제
+    'no-var': 'warn', // var 금지
+    'react/jsx-pascal-case': 'warn', // 컴포넌트 이름은 PascalCase로
+    'react/jsx-key': 'warn', // 반복문으로 생성하는 요소에 key 강제
   },
 };
 
