81912d737fb47fd060c62c58ef526d2c2e5591bb
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-03 02:35:13 +0900

    !HOTFIX!  글로벌 스타일이 컴포넌트 내부에 선언되어 성능 이슈를 야기할 수 있으므로 긴급 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react'
 import { RecoilRoot } from 'recoil'
-
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
 import Routes from './routes/Routes'
 
+// 글로벌 스타일을 활용해 스타일 리셋
+const GlobalStyles = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`
 
 export default function App() {
-
-  // 글로벌 스타일을 활용해 스타일 리셋
-  const GlobalStyles = createGlobalStyle`
-    ${reset};
-    * {
-      box-sizing: border-box;
-    }
-
-    body {
-      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-  `
-
-
 
   return (
     <RecoilRoot>
