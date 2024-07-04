999ad68566b258b41d09fa4112365976c15cc349
Min-byeonghyun <qudgus5125@naver.com>
2024-07-04 23:19:40 +0900

    !HOTFIX! Footer영역 position : fixed로 고정시킨거 뺐습니다

--- src/layout/LayoutFooter.tsx
+++ src/layout/LayoutFooter.tsx
@@ -3,14 +3,12 @@ import styled from "styled-components"
 
 const Wrapper = styled.footer`
   display: flex;
- position: fixed;
- bottom: 0;
- width: 100%;
- height: 90px;
- background-color: rgb(52, 58, 64);
- padding: 20px;
- text-align: center;
- justify-content: center;
+  width: 100%;
+  height: 90px;
+  background-color: rgb(52, 58, 64);
+  padding: 20px;
+  text-align: center;
+  justify-content: center;
 `
 
 const FooterBox = styled.div`
