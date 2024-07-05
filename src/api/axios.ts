1d8f39c1b971da5600d4b035541dca0f7e4a0753
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-06 00:23:02 +0900

    feat : MSW Setup Mock API
    - msw 라이브러리 초기 설치
    - 테스트를 위한 샘플 코드 작성
    
    new feat #14

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // axios instance creation.
 const instance = axios.create({
-  baseURL: 'baseUrl',
+  baseURL: '/api',
+  timeout: 1000,
 });
 
 export default instance;
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,56 @@
+import { http, HttpResponse } from 'msw';
+
+const allPosts = new Map();
+
+export const handlers = [
+  http.get('/posts', () => {
+    // Construct a JSON response with the list of all posts
+    // as the response body.
+    console.log('Captured a "GET /posts" request');
+    return HttpResponse.json(Array.from(allPosts.values()));
+  }),
+
+  // ...the other request handlers.
+  // 포스트 요청 보내는 방법
+  http.post('/posts', async ({ request }) => {
+    // Read the intercepted request body as JSON.
+    const newPost = await request.json();
+
+    allPosts.set(newPost.id, newPost);
+
+    // Don't forget to declare a semantic "201 Created"
+    // response and send back the newly created post!
+    return HttpResponse.json(newPost, { status: 201 });
+  }),
+
+  // 쿠키 읽기
+  http.get('/user', ({ cookies }) => {
+    const { session } = cookies;
+
+    if (!session) {
+      return new HttpResponse(null, { status: 401 });
+    }
+  }),
+
+  // params를 이용해 id 읽기
+  http.delete('/posts/:id', ({ params }) => {
+    // All request path params are provided in the "params"
+    // argument of the response resolver.
+    const { id } = params;
+
+    // Let's attempt to grab the post by its ID.
+    const deletedPost = allPosts.get(id);
+
+    // Respond with a "404 Not Found" response if the given
+    // post ID does not exist.
+    if (!deletedPost) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    // Delete the post from the "allPosts" map.
+    allPosts.delete(id);
+
+    // Respond with a "200 OK" response and the deleted post.
+    return HttpResponse.json(deletedPost);
+  }),
+];
