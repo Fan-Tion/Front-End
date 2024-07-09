73f1474a4bf07ca2e989dcce1fa9d186db914dc5
Min-byeonghyun <qudgus5125@naver.com>
2024-07-10 00:09:08 +0900

    HOTFIX : members 객체삭제

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,22 +1,5 @@
 import { http, HttpResponse } from 'msw';
-
-
-
-const members = new Map([
-  [
-    'Test@email.com', // Map의 Key를 이메일로 저장하면 로그인 요청 Mock API에서 데이터를 빠르게 찾을 수 있을 것...
-    {
-      email: 'Test@email.com',
-      password: 'testPassword123',
-      nickname: '테스트',
-      address: '테스트 주소',
-      profileImage: '',
-    },
-  ],
-]);
-
 import { auctions, members, membersMapType } from './db';
-
 
 export const handlers = [
   // 더미 이미지 URL 가로채지 않게 하기
