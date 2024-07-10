32860544056eef1c1a2f9794279f0292e1cbc795
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-10 22:06:49 +0900

    feat : TOAST UI 툴바 한글로 표시하도록 변경
    # F/U #23

--- src/utils/TextEditor.tsx
+++ src/utils/TextEditor.tsx
@@ -1,15 +1,19 @@
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/i18n/ko-kr' // 툴바 한글로 표시하기 위한 모듈
 
 export default function TextEditor() {
 
-  // 이미지 업로드 기능을 사용하지 못하게 할 예정이므로 툴바를 커스텀함 
+  // 이미지 업로드 기능을 사용하지 못하게 할 예정이므로 툴바 커스텀 
   const toolbarItems = [
     ['heading', 'bold', 'italic', 'strike'],
     ['hr', 'quote'],
     ['ul', 'ol', 'task', 'indent', 'outdent'],
     ['table', 'link'],
   ];
+
+  // 추후 다국어 지원을 할 수 있으므로 변수로 선언해두기
+  const language = 'ko-KR';
 
   return (
     <Editor
@@ -20,6 +24,7 @@ export default function TextEditor() {
       useCommandShortcut={true}
       toolbarItems={toolbarItems}
       hideModeSwitch={true}  // 편집기 모드 체인지 버튼을 숨김
+      language={language}
     />
   )
 }
