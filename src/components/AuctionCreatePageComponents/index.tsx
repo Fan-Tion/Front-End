e81523f8b486dcd0b70a7fdaf0c494ef17ad9cf2
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-12 01:53:18 +0900

    feat :  form input 유효성 검사 추가
    - 각 input 요소의 유효성을 검사할 수 있는 로직을 추가함
    - 요구사항에 충족할 수 있도록 입력 가능한 데이터 타입 정의
    - 입력 가능한 범위를 제한하는 로직 추가
    
    F/U #29

--- src/components/AuctionCreatePageComponents/index.tsx
+++ src/components/AuctionCreatePageComponents/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import ImageUploader from "./ImageUploader";
 import TextEditor from "../../utils/TextEditor";
 import InputArea from "./InputArea";
-
+import { useState } from "react";
 
 const Wrapper = styled.section`
   margin: 20px auto;
@@ -35,13 +35,94 @@ const Button = styled.button`
   width: 150px;
 `
 
+interface formDataType {
+  title: string
+  currentBidPrice: string | number,
+  buyNowPrice: string | number,
+  endDate: string,
+  auctionType: boolean,
+}
+
+interface ValidityType {
+  title: boolean;
+  currentBidPrice: boolean;
+  buyNowPrice: boolean;
+  endDate: boolean;
+}
+
+const getDateRange = () => {
+  const today = new Date();
+  const sixDaysLater = new Date();
+  sixDaysLater.setDate(today.getDate() + 6);
+
+  const pad = (n: number) => `${Math.floor(Math.abs(n))}`.padStart(2, '0');
+  const toISOStringWithLocalOffset = (date: Date) =>
+    date.getFullYear() +
+    '-' + pad(date.getMonth() + 1) +
+    '-' + pad(date.getDate());
+
+  return {
+    minDate: toISOStringWithLocalOffset(today),
+    maxDate: toISOStringWithLocalOffset(sixDaysLater)
+  };
+};
+
 export default function AuctionCreatePageComponents() {
+
+  const [formData, setFormData] = useState<formDataType>({
+    title: '',
+    currentBidPrice: '',
+    buyNowPrice: '',
+    endDate: '',
+    auctionType: false,
+  })
+
+  const [validity, setValidity] = useState<ValidityType>({
+    title: false,
+    currentBidPrice: false,
+    buyNowPrice: false,
+    endDate: false,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+
+    // 가격 입력에 대한 음수 값 방지 및 숫자만 입력되도록 유효성 검사
+    if ((name === "currentBidPrice" || name === "buyNowPrice") && (isNaN(Number(value)) || Number(value) < 0)) {
+      return;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+
+    setValidity({
+      ...validity,
+      [name]: value.trim() !== '',
+    });
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+  }
+
+  const { minDate, maxDate } = getDateRange();
 
   return (
     <Wrapper>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Col>
-          <InputArea />
+          <InputArea
+            onChange={handleChange}
+            formData={formData}
+            validity={validity}
+            minDate={minDate}
+            maxDate={maxDate}
+          />
           {/* 모달로 구현하는 것이 이쁠 것 같음 ... */}
           <ImageUploader />
         </Col>
