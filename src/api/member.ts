f0dc85eb77ef7f9baa1ac9155d91f5151437c134
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-19 14:47:48 +0900

    fix : axios 인터페이스 타입에러 수정
    - 타입에러 수정 위해 타입 명시화
    - 스펠체커 예외 추가

--- src/api/member.ts
+++ src/api/member.ts
@@ -3,7 +3,10 @@ import { axiosInstance } from './axios';
 export const membersApi = {
   signUp: (payload: any) => axiosInstance.post('/members/signup', payload),
   signIn: (payload: any) => axiosInstance.post('/members/signin', payload),
-  requestPasswordReset: (payload: any) =>
+  requestPasswordReset: (payload: {
+    email: string;
+    phoneNumber: string;
+  }): Promise<string> =>
     axiosInstance.post('/members/reset-password-request', payload),
   resetPassword: (payload: any) =>
     axiosInstance.put('/members/reset-password', payload),
