0375681ea267e315bab7fdc7be21e94dbc1fbcfd
Hira-Yuki <cjsdlf44@gmail.com>
2024-07-31 22:56:06 +0900

    !HOTFIX! : 로그인 검사 로직 수정

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,18 +1,20 @@
 import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-//추후 api 연결시, 이부분 서버로부터 로그인 상태를 확인하도록 수정할 예정
-
-const user = {
-  isLoggedIn: !!Cookies.get('Authorization'),
-};
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!Cookies.get('Authorization'))
+
+  useEffect(() => {
+    setIsLoggedIn(!!Cookies.get('Authorization'))
+  }, [Cookies])
+
   //api 를 통해 로그인 상태를 확인하도록 수정할 부분
-  if (user.isLoggedIn === false) {
+  if (isLoggedIn === false) {
     return <Navigate to="/signin" />;
   }
   return <>{children}</>;
