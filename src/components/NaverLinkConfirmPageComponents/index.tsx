3344674580692139290f284b1046a4d1729b0a45
Min-byeonghyun <qudgus5125@naver.com>
2024-08-26 20:51:18 +0900

    수정

--- src/components/NaverLinkConfirmPageComponents/index.tsx
+++ src/components/NaverLinkConfirmPageComponents/index.tsx
@@ -1,32 +1,27 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import { naverLoginApi } from '@api/naverLogin';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
-
 const Wrap = styled.div`
- display : flex;
- justify-content : center;
- align-items : center;
- width : 100%;
- height : 600px;
-
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 600px;
+`;
 
 const Message = styled.div`
-  display : flex;
-  align-items : center;
-  justify-content : center;
-  width : 600px;
-  height : 300px;
-  font-size : 48px;
-`
-
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 800px;
+  height: 500px;
+  font-size: 48px;
+`;
 
 export default function NaverLinkConfirmPage() {
-  const { search }= useLocation();
+  const { search } = useLocation();
   const [statusMessage, setStatusMessage] = useState('연동 확인 중입니다...');
 
   const queryParams = new URLSearchParams(search);
@@ -37,7 +32,10 @@ export default function NaverLinkConfirmPage() {
     const confirmLink = async () => {
       if (linkEmail && uuid) {
         try {
-          const response = await naverLoginApi.linkNaverAccount({ linkEmail, uuid });
+          const response = await naverLoginApi.linkNaverAccount({
+            linkEmail,
+            uuid,
+          });
           console.log(response.data);
           if (response.data.success) {
             setStatusMessage('연동이 성공적으로 완료되었습니다.');
@@ -45,7 +43,9 @@ export default function NaverLinkConfirmPage() {
             setStatusMessage('연동에 실패했습니다. 다시 시도해 주세요.');
           }
         } catch (error) {
-          setStatusMessage('연동 중 오류가 발생했습니다. 나중에 다시 시도해 주세요.');
+          setStatusMessage(
+            '연동 중 오류가 발생했습니다. 나중에 다시 시도해 주세요.',
+          );
           console.error('연동 중 오류 발생:', error);
         }
       }
